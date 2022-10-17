560d7e8a86fe886484782031c89f357d30dc0b03
QIZHONG-noerror <cuihuazq@email.swu.edu.cn>
2022-10-17 14:27:46 +0800

    define methods and think about framework

---
+++ HW1/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    int StudentNumber;
+    String name;
+    int MathScore;
+    int ChineseScore;
+    int EnglishScore;
+
+}
--- HW1/src/StudentManager.java
+++ HW1/src/StudentManager.java
@@ -1,10 +1,35 @@
 public class StudentManager {
-    public void method(){
+    /*打印所有学生信息：
+    包含 “学号”，“姓名”，“语文”，”数学“，”外语“，”总分“，”平均分“
+     */
+    public void printAll(){
         System.out.println("working");
     }
 
-    public void method2(){
+    //根据学号查询学生信息
+    public void getStudentById(String id){
         System.out.println("studying");
     }
+
+    //根据课程类别，分值范围查询返回符合要求的学生列表;
+    public void findByScore(String classType, int min, int max) {
+        System.out.println("studying");
+    }
+    //查询三科成绩平均分在指定范围内的学生列表;
+    public void findByScore(int min, int max){
+        System.out.println("studying");
+    }
+    //返回一个列表，列表中的学生按总分由高到低排序;
+    public void sortByTotal(){
+        System.out.println("studying");
+    }
+    /*
+    将学生信息按 “学号”，“姓名”，“语文”，”数学“，”外语“，”总分“，”平均分“
+    字段排列存另存到参数指定的文件中，字段用逗号隔开;
+     */
+    public void saveAs(String filePath){
+        System.out.println("studying");
+    }
+
 
 }
